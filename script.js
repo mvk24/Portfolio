ea4192ce7b75b3d54a0d7e7d47e81e952ad579e2
Varun Mithyantha <varun.mithyantha@gmail.com>
2025-02-05 15:13:26 +0530

    Experience Updated

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Jr. Software Engineer @ Cognizant", "Hardworker", "Programmer", "Fast Learner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
